b5ec3ba9b700533e12bf28faf81f5ef312c8db2b
TheColdblood <simongregory05@gmail.com>
2024-03-20 15:23:03 +0100

    dolgozat1

--- adatok.js
+++ adatok.js
@@ -1,6 +1,6 @@
 export const RETROAUTOK = [
-    { nev: "Trabant", evjarat: 1975, ar: "500000 ft", kep: "kepek/trabant.jpg" },
-    { nev: "Wartburg", evjarat: 1986, ar: "600000 ft", kep: "kepek/wartburg.jpg" },
-    { nev: "Skoda", evjarat: 1990, ar: "550000 ft", kep: "kepek/skoda.jpg" },
-    { nev: "VAZ", evjarat: 1962, ar: "700000 ft", kep: "kepek/vaz.jpg" }
+    { nev: "Trabant", evjarat: 1975, ar: "500.000 ft", kep: "kepek/trabant.jpg" },
+    { nev: "Wartburg", evjarat: 1986, ar: "600.000 ft", kep: "kepek/wartburg.jpg" },
+    { nev: "Skoda", evjarat: 1990, ar: "550.000 ft", kep: "kepek/skoda.jpg" },
+    { nev: "VAZ", evjarat: 1962, ar: "700.000 ft", kep: "kepek/vaz.jpg" }
 ];
